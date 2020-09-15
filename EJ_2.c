c0d10db4e3cb6d3be95ffccb273b927c268c25e1
Luz255181 <mira_luz_explo@hotmail.com>
2020-09-15 20:35:46 -0300

    Segundo comentario

---
+++ EJ_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int seg = 0, min = 0, horas = 0;
+int run(long int st)
+{
+    seg = (int)st%60;
+    min = (int)st/60;
+    horas = (int)min/60;
+    min = (int)min%60;
+    printf("La convercion a horas, minutos y segundos es: %d hs %d min %d seg", horas, min, seg);
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "EJ_10.c"
+int main()
+{
+    return Palindroma();
+}
